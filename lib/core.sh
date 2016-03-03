c8747791c1469222f47a2579f93c2cc125e91a61
deluxebrain <matt@deluxebrain.com>
2016-03-03 22:08:43 +0000

    Bootstrapper

--- lib/core.sh
+++ lib/core.sh
@@ -1,6 +1,72 @@
 #!/bin/bash
 
-# [ -z "${__CORE+.}" ] && readonly __CORE= || return 0
+[ -z "${__CORE+.}" ] && readonly __CORE= || return 0
+
+#-------------------------------------------------------------
+# Pens
+#+ http://linuxcommand.org/lc3_adv_tput.php
+#-------------------------------------------------------------
+
+# Example usages
+#+ To set for all subsequent output
+#$ echo -en ${WHITE}${ON_RED}
+
+#+ To output text in specific ped
+#$ echo ${WHITE}${ON_RED}...${PEN_RESET}
+
+export STYLE_BOLD="$(tput bold)"
+export STYLE_REV="$(tput rev)"
+export SYTLE_UNDERLINE_START="$(tput smul)"
+export STYLE_UNDERLINE_STOP="$(tput rmul)"
+export STYLE_STANDOUT_START="$(tput smso)"
+export STYLE_STANDOUT_END="$(tput rmso)"
+export STYLE_RESET="$(tput sgr0)"
+
+export BLACK="$(tput setaf 0)"
+export BLUE="$(tput setaf 33)"
+export CYAN="$(tput setaf 37)"
+export GREEN="$(tput setaf 64)"
+export ORANGE="$(tput setaf 166)"
+export PURPLE="$(tput setaf 125)"
+export RED="$(tput setaf 1)"
+export VIOLET="$(tput setaf 61)"
+export WHITE="$(tput setaf 15)"
+export YELLOW="$(tput setaf 136)"
+export NC="$(tput setaf 9)"
+
+export BBLACK=${STYLE_BOLD}${BLACK}
+export BBLUE=${STYLE_BOLD}${BLUE}
+export BCYAN=${STYLE_BOLD}${CYAN}
+export BGREEN=${STYLE_BOLD}${GREEN}
+export BORANGE=${STYLE_BOLD}${ORANGE}
+export BPURPLE=${STYLE_BOLD}${PURPLE}
+export BRED=${STYLE_BOLD}${RED}
+export BVIOLET=${STYLE_BOLD}${VIOLET}
+export BWHITE=${STYLE_BOLD}${WHITE}
+export BYELLOW=${STYLE_BOLD}${YELLOW}
+
+export ON_BLACK="$(tput setab 0)"
+export ON_BLUE="$(tput setab 33)"
+export ON_CYAN="$(tput setab 37)"
+export ON_GREEN="$(tput setab 64)"
+export ON_ORANGE="$(tput setab 166)"
+export ON_PURPLE="$(tput setab 125)"
+export ON_RED="$(tput setab 1)"
+export ON_VIOLET="$(tput setab 61)"
+export ON_WHITE="$(tput setab 15)"
+export ON_YELLOW="$(tput setab 136)"
+export ON_NC="$(tput setab 9)"
+
+export PEN_RESET=${NC}${STYLE_RESET}
+export PEN_WARN=${BWHITE}${ON_ORANGE}
+export PEN_ALERT=${BWHITE}${ON_RED}
+export PEN_HIGHLIGHT=${BYELLOW}${ON_BLUE}
+export PEN_INFO=${BBLUE}${ON_WHITE}
+export PEN_OK=${BGREEN}${ON_WHITE}
+
+#-------------------------------------------------------------
+# Global error handlers
+#-------------------------------------------------------------
 
 function __exit_handler()
 {
@@ -51,8 +117,12 @@ function USE_GLOBAL_ERROR_HANDLER()
 	trap '__error_handler ${LINENO} $?' ERR
 }
 
-_DEBUG=false
-_TRACE=false 
+#-------------------------------------------------------------
+# Script debugging and tracing
+#-------------------------------------------------------------
+
+_DEBUG=false	  # Set to true to echo instead of executing a command
+_TRACE=false	  # Set to true to trace a command to stdout
 function DEBUG()
 {
 	local code
@@ -68,6 +138,10 @@ function DEBUG()
 		eval "$*"
 	fi
 }
+
+#-------------------------------------------------------------
+# Pretty print
+#-------------------------------------------------------------
 
 function msg_info() 
 {
@@ -100,6 +174,10 @@ function fail()
         exit 1
 }
 
+#-------------------------------------------------------------
+# Functional helpers
+#-------------------------------------------------------------
+
 function _join()
 {
 	# Set expansion character
@@ -111,3 +189,4 @@ function _join()
 	# Expand args to "$1c$2c..." where c is first character if IFS
 	echo "$*"
 }
+
